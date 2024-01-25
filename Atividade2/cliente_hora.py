85534ddbe7a35c17569c1bfb33d73ec87ac8278b
CICERO JOSE FERREIRA DE SOUSA SOUSA <cicerojose@finlandia.k05s>
2024-01-25 12:25:05 -0300

    added atividade 3

---
+++ Atividade2/cliente_hora.py
@@ -0,0 +1,24 @@
+import socket
+import threading
+
+def receber_mensagens(sock):
+        data = sock.recv(2048)
+        mensagem = data.decode()
+        #Imprimindo a mensagem recebida
+        print(data.decode())
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  UDP
+    
+server_address = ('localhost', 20001)
+print ("Conectando %s porta %s" % server_address)
+#Conectando ao servidor
+sock.connect(server_address)
+recepcao = threading.Thread(target=receber_mensagens,args=(sock,))
+recepcao.start() 
+
+message = input("Solicite o horário: ")
+#Enviando mensagem ao servidor
+sock.sendall(message.encode('utf-8'))
+
+recepcao.join()
